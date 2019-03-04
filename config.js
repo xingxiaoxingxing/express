241cbb626209e1c290281d5b0ca3e09725d3575d
yuxingxing <2304210953@qq.com>
2019-03-04 18:24:11 +0800

    git tracks changes

--- config.js
+++ config.js
@@ -2,6 +2,10 @@
 var path = require('path');
 
 var config = {
+    site: '1',
+    appid: '18',
+    token: 'MTU1MTQwNjYxMXxOd3dBTkVGRlVUSXlUME5LTkUweVNsZExRVlZWVUZjeVRsVkNVVlJaUlVaUlEwdEROVWRQVTBwQlZrOHlWbFpQTTBOUk4xZEZUMUU9fKc4nYLGvgz5YVmziZJXVvEzwn7A3nwZkFYz2tlAY5Ou',
+    url: 'http://192.168.1.11:8801',
     host: 'localhost:3000',
 
     // 静态文件的目录
--- controller/site.js
+++ controller/site.js
@@ -1,25 +1,31 @@
 var config  = require('../config');
+var request  = require('request');
 
 
 exports.index = function (req, res, next) {
     console.log(config.host, 'host');
-    res.render('index', {
-        dist:"//"+config.host,
-        host:"//"+config.host,
-        // wap:"//"+config.host+"/"+config.staticDir,
-        // UA:req.headers['aijia']||req.headers['user-agent'],
-        // data:JSON.parse(response.body),
-        // gaContent:"/H5/首页",
-    });
+    console.log(config.url+`/service/503/chargeitem/list?site=${config.site}&appid=${config.appid}&token=${config.token}&pagenum=10&pagestart=0`, 'jdiojdi');
+    request(config.url+`/service/503/chargeitem/list?site=${config.site}&appid=${config.appid}&token=${config.token}&pagenum=10&pagestart=0`, function (error, response, body) {
+        console.log(response.statusCode, 'response.statusCode');
+        if (!error && response.statusCode == 200) {
+           let data = [];
+           const content = JSON.parse(response.body);
+           if (content.ret == 0 && content.count > 0) {
+               data = content.data;
+           }
+            res.render('index', {
+                dist:"//"+config.host,
+                host:"//"+config.host,
+                url:config.url,
+                // wap:"//"+config.host+"/"+config.staticDir,
+                // UA:req.headers['aijia']||req.headers['user-agent'],
+                data:data,
+            });
+        }
+    })
 };
 
 exports.one = function (req, res, next) {
     res.render('one/one', {
-        // dist:"//"+config.host+"/"+config.staticDir,
-        // host:"//"+config.host,
-        // wap:"//"+config.host+"/"+config.staticDir,
-        // UA:req.headers['aijia']||req.headers['user-agent'],
-        // data:JSON.parse(response.body),
-        // gaContent:"/H5/首页",
     });
 };
