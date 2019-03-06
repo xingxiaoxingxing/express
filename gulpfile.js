536a5c4f40c4cf90d08b3e0ddfb50230dad57b1d
yuxingxing <2304210953@qq.com>
2019-03-06 14:15:13 +0800

    gulp-server

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,9 @@ var cssUglify = require('gulp-minify-css');
 var imageMin = require('gulp-imagemin');
 var sass = require('gulp-sass');
 var refresh = require('gulp-refresh');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+var nodemon = require('gulp-nodemon');
 
 // 压缩 js 文件
 // 在命令行使用 gulp script 启动此任务
@@ -36,6 +39,37 @@ gulp.task('image',function(){
         .pipe(imageMin({progressive: true}))
         .pipe(gulp.dest('dist/images'))
 })
+
+//这个可以让express启动
+gulp.task("node", function() {
+    nodemon({
+        script: './bin/www',
+        ext: 'js html',
+        env: {
+            'NODE_ENV': 'development'
+        }
+    })
+});
+
+
+gulp.task('server', ["node"], function() {
+    var files = [
+        'views/**/*.html',
+        'views/**/*.ejs',
+        'views/**/*.jade',
+        'public/**/*.*'
+    ];
+
+    //gulp.run(["node"]);
+    browserSync.init(files, {
+        proxy: 'http://localhost:3000',
+        browser: 'chrome',
+        notify: false,
+        port: 3001 //这个是browserSync对http://localhost:3000实现的代理端口
+    });
+
+    gulp.watch(files).on("change", reload);
+});
 
 gulp.task('default',['script','image','css']
 )
